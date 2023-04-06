027047fab2f0737069d20d2b744bf89ee7f7e3d2
Jacob Żyworonek <jzyworonek@gmail.com>
2023-04-06 12:51:40 +0200

    fix bug

--- utils/image_reade_inf.py
+++ utils/image_reade_inf.py
@@ -250,10 +250,10 @@ class ImageReader(object):
         self.images = tf.convert_to_tensor(self.image_list, dtype=tf.string)
         # self.labels = tf.convert_to_tensor(self.label_list, dtype=tf.string)
         # self.edges = tf.convert_to_tensor(self.edge_list, dtype=tf.string)
-        self.queue = tf.compat.v1.train.slice_input_producer([self.images],
+        self.queue = tf.data.Dataset.from_tensor_slices(tuple([self.images]),
                                                               # self.labels,
                                                               # self.edges],
-                                                             shuffle=shuffle)
+                                                        ).shuffle()
         print(self.queue)
         self.image = read_images_from_disk(self.queue, self.input_size,
                                                                   random_scale,
